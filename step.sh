29f5159b1e5d1ec8631bd1ef607a9cb6429d8988
Pavan <mpavankumar@krishservicesgroup.com>
2022-07-12 13:12:03 +0900

    - Modified script to take multiple source branches

--- step.sh
+++ step.sh
@@ -2,30 +2,31 @@
 #set -ex
 
 # This will create the pr from branch to the mention branches
+all_pr_links=""
+createPR() {
+    BRANCH_FROM=$1
+    echo "Creating PR on Bitbucket Repo: ${GIT_BASE_URL} -> ${GIT_PROJECT} -> ${GIT_REPO} with details: "
+    echo "- Title: ${PR_TITLE}"
+    echo "- Description: ${PR_DESCRIPTION}"
+    echo "- Source Branch: ${BRANCH_FROM}"
+    echo "- Target Branch: ${BRANCH_TO}"
+    echo "- Reviewers: ${PR_REVIEWERS}"
 
-echo "Creating PR on Bitbucket Repo: ${GIT_BASE_URL} -> ${GIT_PROJECT} -> ${GIT_REPO} with details: "
-echo "- Title: ${PR_TITLE}"
-echo "- Description: ${PR_DESCRIPTION}"
-echo "- Source Branch: ${BRANCH_FROM}"
-echo "- Target Branch: ${BRANCH_TO}"
-echo "- Reviewers: ${PR_REVIEWERS}"
+    IFS=',' read -r -a REVIEWERS <<<"$PR_REVIEWERS"
+    REVIEWER_LIST=""
+    for name in "${REVIEWERS[@]}"; do
+        if ! [[ ${REVIEWER_LIST} == "" ]]; then
+            REVIEWER_LIST="${REVIEWER_LIST},"
+        fi
+        REVIEWER_LIST="${REVIEWER_LIST}{\"user\": {\"name\": \"${name}\"}}"
+    done
+    echo "- Reviewer PR Input: ${REVIEWER_LIST}"
 
-IFS=',' read -r -a REVIEWERS <<< "$PR_REVIEWERS"
-REVIEWER_LIST=""
-for name in "${REVIEWERS[@]}"
-do
-    if ! [[ ${REVIEWER_LIST} == "" ]]; then
-        REVIEWER_LIST="${REVIEWER_LIST},"
-    fi
-    REVIEWER_LIST="${REVIEWER_LIST}{\"user\": {\"name\": \"${name}\"}}"
-done
-echo "- Reviewer PR Input: ${REVIEWER_LIST}"
-
-echo "Calling bitbucket create PR API..."
-response=$(curl --location --request POST "${GIT_BASE_URL}/rest/api/1.0/projects/${GIT_PROJECT}/repos/${GIT_REPO}/pull-requests" \
---header "Authorization: Bearer ${GIT_ACCESS_TOKEN}" \
---header 'Content-Type: application/json' \
---data-binary "{
+    echo "Calling bitbucket create PR API..."
+    response=$(curl --location --request POST "${GIT_BASE_URL}/rest/api/1.0/projects/${GIT_PROJECT}/repos/${GIT_REPO}/pull-requests" \
+        --header "Authorization: Bearer ${GIT_ACCESS_TOKEN}" \
+        --header 'Content-Type: application/json' \
+        --data-binary "{
     \"title\": \"${PR_TITLE}\",
     \"description\": \"${PR_DESCRIPTION}\",
     \"state\": \"OPEN\",
@@ -56,11 +57,25 @@ response=$(curl --location --request POST "${GIT_BASE_URL}/rest/api/1.0/projects
         ${REVIEWER_LIST}
     ]
 }")
-echo "------------------------ ------------------------------"
-echo "$response"
-echo "PR creation complete...."
+    echo "------------------------ ------------------------------"
+    echo "$response"
+    echo "PR creation complete...."
 
-PATTERN="$GIT_BASE_URL/projects/$GIT_PROJECT/repos/$GIT_REPO/pull-requests/[0-9]*"
-PR_LINK=($(echo "$response" | grep -Eo -1 "$PATTERN"))
-echo "PR Link: $PR_LINK"
-envman add --key PR_LINK --value "$PR_LINK"
+    PATTERN="$GIT_BASE_URL/projects/$GIT_PROJECT/repos/$GIT_REPO/pull-requests/[0-9]*"
+    PR_LINK=($(echo "$response" | grep -Eo -1 "$PATTERN"))
+    echo "Created PR: $PR_LINK"
+    all_pr_links+="$PR_LINK"
+}
+
+# Parse list of source branches to create PR for them
+for source_branch in "${SOURCE_BRANCHES[@]}"; do
+    createPR $source_branch
+done
+
+# Expose PR links environment variable
+if [ -z "$diff_message" ]; then
+    echo "No PRs created"
+else
+    #echo -e "diff between ${source_branch} x ${src_branch}: \n${diff_message}"
+    envman add --key PR_LINKS --value "$all_pr_links"
+fi
